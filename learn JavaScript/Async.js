ab8508fbf483e616207916d33cb506edc11af8ad
deathstalkr <solomon.johnson098@gmail.com>
2022-06-09 23:23:26 +0530

    update

--- learn JavaScript/Async.js
+++ learn JavaScript/Async.js
@@ -62,3 +62,130 @@ let production = () => {
 }
 
 order(3, production) */
+
+// Promise
+/* let stocks = {
+	fruits: ["strawberry", "grapes", "banana", "apple"],
+	liquid: ["water", "ice"],
+	holder: ["cone", "cup", "stick"],
+	toppings: ["chocolate", "peanuts"]
+}
+
+let is_shop_open = true
+
+let order = (time, work) => {
+	return new Promise((resolve, reject) => {
+		if (is_shop_open) {
+			setTimeout(() => {
+				resolve(work())
+			}, time)
+		} else {
+			reject(console.log("Sorry! the shop is closed"))
+		}
+	})
+}
+order(2000, () => console.log(`${stocks.fruits[0]} is selected`))
+
+	.then(() => {
+		return order(0, () => console.log("Production started"))
+	})
+	.then(() => {
+		return order(2000, () => console.log("Fruits are chopped"))
+	})
+	.then(() => {
+		return order(1000, () => console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} are added`))
+	})
+	.then(() => {
+		return order(1000, () => console.log("Machine is started"))
+	})
+	.then(() => {
+		return order(2000, () => console.log(`Ice-cream is placed over the ${stocks.holder[0]}`))
+	})
+	.then(() => {
+		return order(3000, () => console.log(`${stocks.toppings[1]} is added as toppings`))
+	})
+	.then(() => {
+		return order(2000, () => console.log("Ice-cream is served"))
+	})
+
+	.catch(() => console.log("Customer left"))
+
+	.finally(() => console.log("Day ended. Shop is closed")) */
+
+// Async/Await
+/* 
+let select_toppings = () => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(console.log("What toppings would you like?"))
+		}, 3000)
+	})
+}
+
+async function kitchen() {
+	console.log("A")
+	console.log("B")
+	console.log("C")
+	await select_toppings()
+	console.log("D")
+	console.log("E")
+
+}
+kitchen()
+console.log("task X")
+console.log("task Y")
+console.log("task Z") */
+
+let stocks = {
+	fruits: ["strawberry", "grapes", "banana", "apple"],
+	liquid: ["water", "ice"],
+	holder: ["cone", "cup", "stick"],
+	toppings: ["chocolate", "peanuts"]
+}
+
+let is_shop_open = false
+let time = (ms) => {
+	return new Promise((resolve, reject) => {
+		if (is_shop_open) {
+			setTimeout(resolve,ms)
+		} else {
+			reject(console.log("Shop is closed"))
+		}
+	})
+}
+
+async function kitchen() {
+	try {
+		await time(2000)
+		console.log(`${stocks.fruits[0]} is selected`)
+
+		// await time(0000)
+		console.log("start the production")
+
+		await time(2000)
+		console.log("Fruits are chopped")
+
+		await time(1000)
+		console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} is added`)
+
+		await time(1000)
+		console.log("Machine is started")
+
+		await time(2000)
+		console.log(`Ice-cream is placed over the ${stocks.holder[0]}`)
+
+		await time(3000)
+		console.log(`${stocks.toppings[0]} is added as toppings`)
+
+		await time(2000)
+		console.log("Ice-cream is served")
+
+
+	} catch (error){
+		console.log("Customer left", error)
+	} finally {
+		console.log("Day ended. Shop closed")
+	}
+}
+
+kitchen()
