4f539f382cbf1aa9614cd3baf998dffa19a3008c
deathstalkr <solomon.johnson098@gmail.com>
2022-06-11 23:02:29 +0530

    ColorFlipper

--- javascript-basic-projects/01-color-flipper/setup/app.js
+++ javascript-basic-projects/01-color-flipper/setup/app.js
@@ -1,1 +1,12 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn")
+const color =document.querySelector(".color")
+
+btn.addEventListener("click", function() {
+    
+    let randonNumber = Math.floor(Math.random()*colors.length)
+    document.body.style.backgroundColor = colors[randonNumber]
+    color.textContent = colors[randonNumber]
+
+})
+
